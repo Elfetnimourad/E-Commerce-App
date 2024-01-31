5474370035147a240248ffa44a966657b267f81c
Elfetnimourad <moradtao2000@gmail.com>
2024-01-31 02:19:12 +0100

    Second commit

--- src/component/DiscoverUs.test.js
+++
@@ -1,8 +0,0 @@
-import { screen, render, fireEvent } from "@testing-library/react";
-import { DiscoverUs } from "./DiscoverUs";
-
-test("should show the product list", () => {
-  render(<DiscoverUs />);
-  const headElem = screen.getByText("About Us"); // Assuming you have a test ID set
-  expect(headElem).toBeInTheDocument();
-});
